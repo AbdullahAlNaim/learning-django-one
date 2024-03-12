6e9c53df924a80d448a17d5dceea66641daf9fea
Abdullah Naim <aanaim96@gmail.com>
2024-03-11 23:58:24 -0400

    Add files via upload

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.db.models import F
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -15,6 +17,7 @@ def index(request):
   return HttpResponse(template.render(context, request))
 
 
+
 def detail(request, question_id):
   try:
     question = Question.objects.get(pk=question_id)
@@ -22,11 +25,27 @@ def detail(request, question_id):
     raise Http404("Question does not exist")
   return render(request, "polls/detail.html", {"question": question})
 
+
+
 def results(request, question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, "poll/results.html", {"question": question})
+
+
 
 def vote(request, question_id):
-  selected_choice.votes = ("votes") + 1
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render(
+      request,
+      "poll/detail.html",
+      {
+        "question": question,
+        "error_message": "You didn't select a choice.",
+      },
+    )
+  selected_choice.votes = F("votes") + 1
   selected_choice.save()
   return HttpResponse(reverse("poll:results", args=(question_id,)))
