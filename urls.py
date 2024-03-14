9b65e7ed58f80279919e3be590e48d20d74f7060
Abdullah Naim <aanaim96@gmail.com>
2024-03-14 00:15:30 -0400

    Add files via upload
    fixed template reference issue. Had an extra s and shown in comments change

--- urls.py
+++ urls.py
@@ -5,7 +5,12 @@ from . import views
 app_name = "poll"
 urlpatterns = [
   path("", views.IndexView.as_view(), name="index"),
-  path("<int:pk>/", views.views.DetailView.as_view(), name="detail"),
+  path("<int:pk>/", views.DetailView.as_view(), name="detail"),
   path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
   path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+# my dir
+# C:\Users\aanai\Desktop\Developer\python\django_one\mysite\poll\templates\polls
+# app search
+# C:\Users\aanai\Desktop\Developer\python\django_one\mysite\poll\templates\poll\detail.html 
